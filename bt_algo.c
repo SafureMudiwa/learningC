521476362849f97f56bcaf7c47db047aa03c6c46
Safure Mudiwa <mudiwasafure01@gmail.com>
2026-03-29 22:11:34 +0200

    v1.0.0 better skeleton function

--- bt_algo.c
+++ bt_algo.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 
-void backtrack(char "*")
+void backtrack(char *str, char *res, int index, int resIndex) {
+    if (str[index] == '\0'){
+        res[resIndex] = '\0';
+        printf("%s", res);
+        return;
+    }
+    
+
+}
